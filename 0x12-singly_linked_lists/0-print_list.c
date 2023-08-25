20e2efb1ee4e1e9fbec401c5ab86d527657f0ce3
Christoph9369 <mojuyefelix@gmail.com>
2023-08-24 17:09:50 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_list / prints the elements of a linked list
+ * print_list - prints the elements of a linked list
  * @h: pointer to the list_t list
  *
  * Return: the number of nodes printed
