52762693717cd5d6cfd65fc8b608208e21ec7785
Christoph9369 <mojuyefelix@gmail.com>
2023-11-02 12:35:41 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read a text file and prints the letters.
- * @filename: text file 
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
